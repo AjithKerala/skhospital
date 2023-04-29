51bc74f0ba05b6769bca060761616ad6d1e4a302
Ajith <ajithmurali499@gmail.com>
2023-04-29 22:53:44 +0530

    changed template and static postions

--- skapp/urls.py
+++ skapp/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.static import static
 from django.urls import path
 from skapp import views
 urlpatterns = [
+         path('',views.home,name='home')
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
